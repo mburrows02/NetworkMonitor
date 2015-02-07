865e4fe4e289d9e4c35cd7ac7d82497eb4d4bac5
Michelle Burrows <mburrows02@gmail.com>
2015-02-07 15:24:35 -0500

    HTTP message parsing ready but failing due to classpath conflict.

--- app/src/main/java/ca/carleton/michelleburrows/networkmonitor/MainActivity.java
+++ app/src/main/java/ca/carleton/michelleburrows/networkmonitor/MainActivity.java
@@ -2,6 +2,7 @@ package ca.carleton.michelleburrows.networkmonitor;
 
 import android.app.ListFragment;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -16,10 +17,29 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.DataInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.gatech.sjpcap.IPPacket;
+import edu.gatech.sjpcap.Packet;
+import edu.gatech.sjpcap.PcapParser;
+import edu.gatech.sjpcap.TCPPacket;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.io.DefaultHttpRequestParser;
+import org.apache.http.impl.io.DefaultHttpResponseParser;
+import org.apache.http.impl.io.HttpTransportMetricsImpl;
+import org.apache.http.impl.io.SessionInputBufferImpl;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -32,6 +52,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.enableDefaults();
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -136,6 +157,7 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String file = FILE_DIR + ((TextView) view).getText();
+                Log.v(TAG, "Opening file " + file);
                 Bundle bundle = new Bundle();
                 bundle.putString("filename", file);
                 Fragment newFrag = new PacketListFragment();
@@ -172,9 +194,63 @@ public class MainActivity extends ActionBarActivity {
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
 
-            //TODO load file and parse
+            Log.v(TAG, "Parsing " + openFile);
+            List<String> packetList = new ArrayList<String>();
 
-            String[] packets = new String[]{"Hello", "World", openFile};
+            PcapParser parser = new PcapParser();
+            parser.openFile(openFile);
+            Packet packet = parser.getPacket();
+            while (packet != Packet.EOF) {
+                Log.v(TAG, "Parsing packet " + packetList.size());
+                if (!(packet instanceof IPPacket)) {
+                    packet = parser.getPacket();
+                    continue;
+                }
+                IPPacket ipPack = (IPPacket) packet;
+                boolean outgoing = isLocalAddress(ipPack.src_ip);
+                String host = outgoing?ipPack.dst_ip.getCanonicalHostName():ipPack.src_ip.getCanonicalHostName();
+                String type = outgoing?"Request to: ":"Response from: ";
+                Log.v(TAG, "\t" + type + host);
+
+                if (packet instanceof TCPPacket) {
+                    TCPPacket tcpPack = (TCPPacket) ipPack;
+                    byte[] data = tcpPack.data;
+                    if (outgoing) {
+                        SessionInputBufferImpl inBuffer = new SessionInputBufferImpl(new HttpTransportMetricsImpl(), data.length);
+                        InputStream inStream = new ByteArrayInputStream(data);
+                        inBuffer.bind(inStream);
+                        DefaultHttpRequestParser reqParser = new DefaultHttpRequestParser(inBuffer);
+                        try {
+                            HttpRequest req = (HttpRequest) reqParser.parse();
+                            packetList.add(req.getRequestLine().getMethod() + ": " + req.getRequestLine().getUri());
+                            Log.v(TAG, "Request line: " + req.getRequestLine().toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (HttpException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        SessionInputBufferImpl inBuffer = new SessionInputBufferImpl(new HttpTransportMetricsImpl(), data.length);
+                        InputStream inStream = new ByteArrayInputStream(data);
+                        inBuffer.bind(inStream);
+                        DefaultHttpResponseParser respParser = new DefaultHttpResponseParser(inBuffer);
+                        try {
+                            HttpResponse resp = (HttpResponse) respParser.parse();
+                            packetList.add(resp.getStatusLine().getStatusCode() + " " + resp.getStatusLine().getReasonPhrase());
+                            Log.v(TAG, "Status line: " + resp.getStatusLine().toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (HttpException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                //packetList.add(type + host);
+                packet = parser.getPacket();
+            }
+
+            String[] packets = packetList.toArray(new String[packetList.size()]);
 
             ListView list = (ListView)getActivity().findViewById(R.id.packet_listview);
             list.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1, packets));
@@ -187,5 +263,17 @@ public class MainActivity extends ActionBarActivity {
                 //TODO
             }
         };
+
+        private boolean isLocalAddress(InetAddress addr) {
+            if (addr.isAnyLocalAddress() || addr.isLoopbackAddress()) {
+                return true;
+            }
+
+            try {
+                return NetworkInterface.getByInetAddress(addr) != null;
+            } catch (SocketException e) {
+                return false;
+            }
+        }
     }
 }
